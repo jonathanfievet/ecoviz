1c0486894fed65349fb05db0e36cadc67a8cdde9
Jonathan <jonathan.fievetm@gmail.com>
2019-03-15 12:57:35 +0100

    Small refactoring about a missing class.

---
+++ ecoviz-service/src/main/java/org/ecoviz/domain/dto/TagDto.java
@@ -0,0 +1,21 @@
+package org.ecoviz.domain.dto;
+
+public class TagDto {
+
+    private String id;
+
+    private String name;
+
+    public void setId(String id) { this.id = id; }
+
+    public String getId() { return this.id; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getName() { return this.name; }
+
+    @Override
+    public String toString() {
+        return id + ", " + name ;
+    }
+}
